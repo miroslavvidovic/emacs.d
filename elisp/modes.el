07e612f5acbddd16d2ea4d54d15f4eb22779db14
Miroslav Vidović <miroslav-vidovic@hotmail.com>
2017-03-06 14:32:42 +0100

    Web mode, and more vim like keybindings

--- elisp/modes.el
+++ elisp/modes.el
@@ -6,7 +6,7 @@
 ;
 ;------------------------------------------------------------------------------
 
-;; Evil mode, plugins and settings
+; Evil mode, plugins and settings
 ;------------------------------------------------------------------------------
 (require 'evil)
 (evil-mode t)                                 ; Enable evil mode 
@@ -26,9 +26,17 @@
 (setq evil-replace-state-cursor '("red" bar))
 (setq evil-operator-state-cursor '("red" hollow))
 
+; Move betewen panes with control-direction like in vim-tmux 
+(define-key evil-motion-state-map (kbd "C-h") 'evil-window-left)
+(define-key evil-motion-state-map (kbd "C-j") 'evil-window-down)
+(define-key evil-motion-state-map (kbd "C-k") 'evil-window-up)
+(define-key evil-motion-state-map (kbd "C-l") 'evil-window-right)
+
 ; Mappings with leader
 (evil-leader/set-key
-  "ig" 'highlight-indent-guides-mode          ; Toggle indent guides with leader+ig
+  "a"  'ag                                  ; Ack like search
+  "ig" 'highlight-indent-guides-mode        ; Toggle indent guides with leader+ig
+  "kb" 'kill-buffer                         ; Kill - close a buffer
   "b"  'helm-buffers-list;                  ; List buffers
   "m"  'neotree-toggle                      ; Toggle the neo tree
   "ff" 'projectile-find-file                ; Fuzzy find with projectile
@@ -38,7 +46,25 @@
   "q"  'avy-goto-word-1                     ; Easy motion to word
   "l"  'avy-goto-line                       ; Easy motion to line
   "e"  'flycheck-list-errors                ; List all errors in the buffer
+  "sh" 'shell-command                       ; Run a shell command for emacs
+  "x"  'delete-window                       ; Close a window
+  "sp" 'flyspell-mode                       ; Spell checker
+  "l"  'evil-search-highlight-persist-remove-all ; Remove highlighted search results
+  "d"  'dired ; Dired file manager
+  "rn" 'ranger ; ranger file manager
+  "t"  'term ; terminal emulator
+  "ps" 'perspective-switch                  ; Switch to perspective
+  "p"  'counsel-find-file
 )
+
+; Vim style keybinding for commenting
+(define-key evil-normal-state-map "gcc" 'evilnc-comment-or-uncomment-lines)
+(define-key evil-visual-state-map "gcc" 'evilnc-comment-or-uncomment-lines)
+(define-key evil-visual-state-map "lf" 'helm-semantic-or-imenu)
+
+; (ivy-mode 1)
+; (setq ivy-use-virtual-buffers t)
+; (setq enable-recursive-minibuffers t)
 
 ;; Autocomplete
 ;------------------------------------------------------------------------------
@@ -55,7 +81,12 @@
 ; Powerline
 ;------------------------------------------------------------------------------
 ; Start powerline with the center theme
-(powerline-center-theme)                                
+; (powerline-center-theme)                                
+
+; Spaceline
+;------------------------------------------------------------------------------
+(require 'spaceline-config)
+(spaceline-emacs-theme)
 
 ; Avy
 ;------------------------------------------------------------------------------
@@ -90,24 +121,20 @@
 
 ; NeoTree
 ;------------------------------------------------------------------------------
-; ; NeoTree keybindings with evil mode
-; (global-set-key [f8] 'neotree-toggle)
+; NeoTree keybindings with evil mode
+(global-set-key [f8] 'neotree-toggle)
 
-; (setq projectile-switch-project-action 'neotree-projectile-action)
-;   (add-hook 'neotree-mode-hook
-;     (lambda ()
-;       (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
-;       (define-key evil-normal-state-local-map (kbd "I") 'neotree-hidden-file-toggle)
-;       (define-key evil-normal-state-local-map (kbd "z") 'neotree-stretch-toggle)
-;       (define-key evil-normal-state-local-map (kbd "R") 'neotree-refresh)
-;       (define-key evil-normal-state-local-map (kbd "m") 'neotree-rename-node)
-;       (define-key evil-normal-state-local-map (kbd "c") 'neotree-create-node)
-;       (define-key evil-normal-state-local-map (kbd "d") 'neotree-delete-node)
+(setq projectile-switch-project-action 'neotree-projectile-action)
+  (add-hook 'neotree-mode-hook
+    (lambda ()
+      (define-key evil-normal-state-local-map (kbd "I") 'neotree-hidden-file-toggle)
+      (define-key evil-normal-state-local-map (kbd "z") 'neotree-stretch-toggle)
+      (define-key evil-normal-state-local-map (kbd "R") 'neotree-refresh)
+      (define-key evil-normal-state-local-map (kbd "m") 'neotree-rename-node)
+      (define-key evil-normal-state-local-map (kbd "c") 'neotree-create-node)
+      (define-key evil-normal-state-local-map (kbd "d") 'neotree-delete-node)
 
-;       (define-key evil-normal-state-local-map (kbd "s") 'neotree-enter-vertical-split)
-;       (define-key evil-normal-state-local-map (kbd "S") 'neotree-enter-horizontal-split)
-
-;       (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
+      (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
 
 ; Helm
 ;------------------------------------------------------------------------------
@@ -115,44 +142,40 @@
 (require 'helm-config)
 
 ; Javascript mode js2-mode
+;------------------------------------------------------------------------------
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 ; Python-mode
+;------------------------------------------------------------------------------
 (autoload 'python-mode "python-mode" "Python Mode." t)
  (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
  (add-to-list 'interpreter-mode-alist '("python" . python-mode))
+
 ; Jedi
+;------------------------------------------------------------------------------
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)                 ; optional
 ;; After this run jedi:install-server in emacs
 
-; ;; auto complete default
-; ; (global-auto-complete-mode t)
-; ; (ac-config-default)
+; Startup screen dashboard
+;------------------------------------------------------------------------------
+(require 'dashboard)
+(dashboard-setup-startup-hook)
 
-; ;; web mode
-; (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+; PHP
+;------------------------------------------------------------------------------
+(require 'php-mode)
+(eval-after-load 'php-mode
+  '(require 'php-ext))
+
+; Web Mode
+;------------------------------------------------------------------------------
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 ; (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-; (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-; (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-; (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-; (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-; (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-
-; ;; Display auto-complete in helm with ctrl-space
-; ; (global-set-key (kbd "C-<SPC>") 'ac-complete-with-helm)
-; ; (define-key ac-complete-mode-map (kbd "C-<SPC>") 'ac-complete-with-helm)
-
-; ; ;;PHP
-; ; (require 'php-mode)
-; ; (eval-after-load 'company
-; ;   '(progn
-; ;      (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
-; ;      (define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)))
-
-; ; (eval-after-load 'company
-; ;       (lambda ()
-; ;         (set-face-attribute
-; ;          'company-preview
-; ;          nil
-; ;          :background (face-attribute 'company-preview-common :background))))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
