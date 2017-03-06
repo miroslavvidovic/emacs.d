4d80a2b3bf0af46ffbcc3f3d5aa0e5fc26f38499
Miroslav Vidović <miroslav-vidovic@hotmail.com>
2017-03-06 14:30:49 +0100

    More modes

--- init.el
+++ init.el
@@ -6,22 +6,26 @@
 ;
 ;------------------------------------------------------------------------------
 
-;; Package management section
+; Package management section
 (require 'package)
 
-; List the packages you want
+; List of used packages
 (setq package-list '(evil                                 ; Vim mode in emacs
                      evil-leader                          ; Vim leader key mode
                      evil-surround                        ; Vim like surround
                      evil-search-highlight-persist        ; Higlight search like Vim
                      evil-nerd-commenter                  ; Easy comment
+
+                     ; Themes
                      dracula-theme                        ; Theme dracula
                      gruvbox-theme                        ; Theme gruvbox
+                     zenburn-theme                        ; Theme zenburn
+
+                     autopair                             ; Auto pair braces and quotes 
                      auto-complete                        ; Autocompletion
-                     powerline                            ; Vim style powerline
+                     spaceline                            ; Statusline from spacemacs
                      yasnippet                            ; Snippets
                      flycheck                             ; Linters
-                     autopair                             ; Auto pair braces and quotes 
                      highlight-indent-guides              ; Indent guidelines
                      rainbow-delimiters                   ; Rainbow colors
                      neotree                              ; NerdTree clone
@@ -29,30 +33,39 @@
                      projectile                           ; Projectile
                      helm                                 ; Helm
                      helm-ag                              ; Ag (silver searcher) in helm
+                     ivy                                  ; Lightweight alternative to helm
+                     counsel                              ; Ivy tools
+                     dashboard                            ; Welcome screen
+                     helm-dash                            ; Search dash docsets from emacs
+                     sr-speedbar                          ; Alternative to tagbar
+                     perspective                          ; Manage perspectives (workspaces)
 
-                     ;Modes
+                     ; Programming modes
                      cc-mode                              ; Mode for c
                      php-mode                             ; Mode for php
                      js2-mode                             ; Mode for javascript
-                     python-mode
+                     python-mode                          ; Mode for python
                      jedi                                 ; Autocomplete for python
-                     haskell-mode
+                     haskell-mode                         ; Mode for haskell
+                     json-mode                            ; Mode for json
+                     web-mode                             ; Mode for html...
 
                      ;Git
                      git-gutter                           ; Git changes
                      magit                                ; Git interaction
+                     git-timemachine                      ; Step through git history
                      ))
 
 ; Package repositories
 (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t) 
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")) 
 
 ; Activate all the packages 
 (package-initialize)
 
-; Update your local package index
+; Update local package index
 (unless package-archive-contents
 (package-refresh-contents))
 
@@ -61,15 +74,8 @@
   (unless (package-installed-p package)
 (package-install package)))
 
-; ; List of required packages
-;                           ; 'use-package                          ; Control package loading
-;                           ; 'popup
-;                           ; 'web-mode
-;                           ; 'ac-php
-; )
-
-;; Add elisp conf files to path
+; Add elisp conf files to path
 (add-to-list 'load-path "~/.emacs.d/elisp")
 (load-library "general-settings")
 (load-library "modes")
-; (load-library "custom")
+(load-library "custom")
