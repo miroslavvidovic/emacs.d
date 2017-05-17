7676cd59667ba67c07be91f010be1a14b7322272
Miroslav Vidović <miroslav-vidovic@hotmail.com>
2017-05-17 15:26:32 +0200

    Emacs custom settings

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("d320493111089afba1563bc3962d8ea1117dd2b3abb189aeebdc8c51b5517ddb" default)))
+ '(exec-path
+   (quote
+    ("/usr/local/sbin" "/usr/local/bin" "/usr/sbin" "/usr/bin" "/sbin" "/bin" "/usr/games" "/usr/local/games" "/snap/bin" "/usr/lib/jvm/oracle_jdk8/bin" "/usr/lib/jvm/oracle_jdk8/db/bin" "/usr/lib/jvm/oracle_jdk8/jre/bin" "/usr/lib/emacs/24.5/x86_64-linux-gnu" "/opt/clojure-leiningen")))
+ '(initial-frame-alist (quote ((fullscreen . maximized)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
