036604555596b5b9d6ce5f7023e9de7cb2d86857
Miroslav Vidović <miroslav-vidovic@hotmail.com>
2017-05-26 20:36:09 +0200

    Complete rewrite with use-package and new modes

--- elisp/modes.el
+++ elisp/modes.el
@@ -1,181 +1,372 @@
-;------------------------------------------------------------------------------
-;
-; Miroslav Vidović
-;
-; emacs configuration
-;
-;------------------------------------------------------------------------------
+;;; modes.el --- Miroslav Vidović emacs modes file
+;;; Commentary:
+;;
 
-; Evil mode, plugins and settings
-;------------------------------------------------------------------------------
-(require 'evil)
-(evil-mode t)                                 ; Enable evil mode 
+;;; Code:
 
-(global-evil-leader-mode)                     ; Enable evil leader key 
-(evil-leader/set-leader "č")                  ; Set the evil mode leader key
+;; Extensible and user friendly startup screen
+(use-package dashboard
+    :ensure t
+    :init
+    ;; The title
+    (setq dashboard-banner-logo-title "MIROSLAV VIDOVIĆ")
+    ;; Customize what will be displayed
+    (setq dashboard-items '((recents . 15)))
+    :config
+    (dashboard-setup-startup-hook))
 
-(global-evil-search-highlight-persist t)      ; Evil mode vim like search highlighting
+;; Hide mode info from mode(status) line. Used as a macro in use-package
+(use-package diminish
+    :ensure t)
 
-(global-evil-surround-mode 1)                 ; Enable evil surround mode
+;; General for keybindings
+(use-package general
+    :ensure t
+    :init
+    (setq my-leader "č")
+    :config
+    (general-evil-setup))
 
-; Evil mode cursor colors
-(setq evil-emacs-state-cursor '("red" box))
-(setq evil-normal-state-cursor '("green" box))
-(setq evil-visual-state-cursor '("orange" box))
-(setq evil-insert-state-cursor '("red" bar))
-(setq evil-replace-state-cursor '("red" bar))
-(setq evil-operator-state-cursor '("red" hollow))
+;; General keybindings - not mode based
+(general-nmap :prefix my-leader
+              "kb" 'kill-buffer)
+(general-nmap :prefix my-leader
+              "x" 'delete-window)
+(general-nmap :prefix my-leader
+              "sh" 'shell-command)
+(general-nmap :prefix my-leader
+              "te" 'term)
+(general-nmap :prefix my-leader
+              "vo" 'find-tag-other-window)
+(general-nmap :prefix my-leader
+              "tg" 'create-tags)
+(general-nmap :prefix my-leader
+              "cc" 'compile)
+(general-nmap :prefix my-leader
+              "gz" 'zeal-at-point)
+(general-nmap :prefix my-leader
+              "df" 'dired)
 
-; Move betewen panes with control-direction like in vim-tmux 
-(define-key evil-motion-state-map (kbd "C-h") 'evil-window-left)
-(define-key evil-motion-state-map (kbd "C-j") 'evil-window-down)
-(define-key evil-motion-state-map (kbd "C-k") 'evil-window-up)
-(define-key evil-motion-state-map (kbd "C-l") 'evil-window-right)
+;; Make emacs behave like Vim
+(use-package evil
+    :ensure t
+    :init
+    ;; Evil mode cursor colors
+    (setq evil-emacs-state-cursor '("red" box))
+    (setq evil-normal-state-cursor '("green" box))
+    (setq evil-visual-state-cursor '("orange" box))
+    (setq evil-insert-state-cursor '("red" bar))
+    (setq evil-replace-state-cursor '("red" bar))
+    (setq evil-operator-state-cursor '("red" hollow))
+    ;; Keybinding until general is set up
+    ;; Move betewen panes with control-direction like in vim-tmux 
+    :config
+    (define-key evil-motion-state-map (kbd "C-h") 'evil-window-left)
+    (define-key evil-motion-state-map (kbd "C-j") 'evil-window-down)
+    (define-key evil-motion-state-map (kbd "C-k") 'evil-window-up)
+    (define-key evil-motion-state-map (kbd "C-l") 'evil-window-right)
+    (evil-mode t))
 
-; Mappings with leader
-(evil-leader/set-key
-  "a"  'ag                                  ; Ack like search
-  "ig" 'highlight-indent-guides-mode        ; Toggle indent guides with leader+ig
-  "kb" 'kill-buffer                         ; Kill - close a buffer
-  "b"  'helm-buffers-list;                  ; List buffers
-  "m"  'neotree-toggle                      ; Toggle the neo tree
-  "ff" 'projectile-find-file                ; Fuzzy find with projectile
-  "o"  'helm-find-files                     ; Open a file with helm
-  "rb" 'rainbow-delimiters-mode             ; Toggle rainbow delimiters
-  "ci" 'evilnc-comment-or-uncomment-lines   ; Comment selected lines
-  "q"  'avy-goto-word-1                     ; Easy motion to word
-  "l"  'avy-goto-line                       ; Easy motion to line
-  "e"  'flycheck-list-errors                ; List all errors in the buffer
-  "sh" 'shell-command                       ; Run a shell command for emacs
-  "x"  'delete-window                       ; Close a window
-  "sp" 'flyspell-mode                       ; Spell checker
-  "l"  'evil-search-highlight-persist-remove-all ; Remove highlighted search results
-  "d"  'dired ; Dired file manager
-  "rn" 'ranger ; ranger file manager
-  "t"  'term ; terminal emulator
-  "ps" 'perspective-switch                  ; Switch to perspective
-  "p"  'counsel-find-file
-)
+;; Just like vim surround
+(use-package evil-surround
+    :ensure t
+    :config
+    (global-evil-surround-mode t))
 
-; Vim style keybinding for commenting
-(define-key evil-normal-state-map "gcc" 'evilnc-comment-or-uncomment-lines)
-(define-key evil-visual-state-map "gcc" 'evilnc-comment-or-uncomment-lines)
-(define-key evil-visual-state-map "lf" 'helm-semantic-or-imenu)
+;; Vim commenting function
+(use-package evil-nerd-commenter
+    :ensure t
+    :config
+    (define-key evil-normal-state-map "gcc" 'evilnc-comment-or-uncomment-lines)
+    (define-key evil-visual-state-map "gcc" 'evilnc-comment-or-uncomment-lines))
 
-; (ivy-mode 1)
-; (setq ivy-use-virtual-buffers t)
-; (setq enable-recursive-minibuffers t)
+;; Persist search highlighting
+(use-package evil-search-highlight-persist
+    :ensure t
+    :config
+    (global-evil-search-highlight-persist t)
+    :general
+    (general-nmap :prefix my-leader
+                  "cl" 'evil-search-highlight-persist-remove-all))
 
-;; Autocomplete
-;------------------------------------------------------------------------------
-(ac-config-default)
-; (define-key ac-mode-map (kbd "C-n") 'auto-complete)
-(ac-set-trigger-key "TAB")
-; (setq ac-auto-start nil)
+;; Auto pair braces and quotes
+(use-package smartparens-config
+    :ensure smartparens
+    :diminish smartparens-mode
+    :config
+    (smartparens-global-mode t))
 
-; Snippets
-;------------------------------------------------------------------------------
-(require 'yasnippet)
-(yas-global-mode 1)
+;; Autocompletion
+(use-package auto-complete
+    :ensure t
+    :init
+    (setq ac-auto-start nil)
+    :config
+    (ac-config-default)
+    (ac-set-trigger-key "TAB"))
 
-; Powerline
-;------------------------------------------------------------------------------
-; Start powerline with the center theme
-; (powerline-center-theme)                                
+;; Jump to things
+(use-package avy
+    :general
+    (general-nmap :prefix my-leader
+                  "q" 'avy-goto-word-1)
+    (general-nmap :prefix my-leader
+                  "l" 'avy-goto-line)
+    :ensure t)
 
-; Spaceline
-;------------------------------------------------------------------------------
-(require 'spaceline-config)
-(spaceline-emacs-theme)
+;; Spaceline
+; (use-package spaceline-config
+;     :ensure spaceline
+;     :config
+;     (spaceline-emacs-theme))
 
-; Avy
-;------------------------------------------------------------------------------
-; Shortcuts for word and line jump in vim evil keybindings
+;; Smart mode line
+(use-package smart-mode-line
+    :ensure t
+    :init
+    (setq sml/theme 'dark)
+    (setq sml/no-confirm-load-theme t)
+    :config
+    (sml/setup))
 
-; Flycheck
-;------------------------------------------------------------------------------
-(require 'flycheck)
-(global-flycheck-mode)
+;; Alternative to gitgutter and fringe
+(use-package diff-hl
+    :ensure t
+    ; :init (global-diff-hl-mode)
+    :config
+    (add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
+    (add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode)
+    (add-hook 'dired-mode-hook 'diff-hl-dired-mode))
 
-; Enable autopairs in all buffers
-;------------------------------------------------------------------------------
-(autopair-global-mode) 
+;; Step thorough git history
+(use-package git-timemachine
+    :ensure t)
 
-; Rainbow delimiters
-;------------------------------------------------------------------------------
-; Not enabled on startup, shortcut for toggle is set up in vim evil keybindings
+;; Intaract with git from emacs
+(use-package magit
+    :ensure t)
 
-; projectile
-;------------------------------------------------------------------------------
-; Not enabled on startup, shortcuts for commands are set up in vim evil keybindings
+;; Snippets
+(use-package yasnippet
+    :ensure t
+    :diminish yas-minor-mode
+    :config
+    (yas-global-mode 1))
 
-; Higlight indent guides
-;------------------------------------------------------------------------------
-; Not enabled on startup, shortcut for enabling is set up in vim evil keybindings
-(setq highlight-indent-guides-method 'column)
+;; Clean whitespaces
+(use-package whitespace-cleanup-mode
+    :ensure t)
 
-; GitGutter
-;------------------------------------------------------------------------------
-; ;; GitGutter activation
-(global-git-gutter-mode +1) 
+;; Syntax checking
+(use-package flycheck
+    :ensure t
+    :general
+    (general-nmap :prefix my-leader
+                  "e" 'flycheck-list-errors)
+    ; :diminish flycheck-mode
+    :config
+    (global-flycheck-mode))
 
-; NeoTree
-;------------------------------------------------------------------------------
-; NeoTree keybindings with evil mode
-(global-set-key [f8] 'neotree-toggle)
+;; Undo tree
+(use-package undo-tree
+    :ensure t
+    :general
+    (general-nmap :prefix my-leader
+                  "uv" 'undo-tree-visualize)
+    :diminish undo-tree-mode)
 
-(setq projectile-switch-project-action 'neotree-projectile-action)
-  (add-hook 'neotree-mode-hook
-    (lambda ()
-      (define-key evil-normal-state-local-map (kbd "I") 'neotree-hidden-file-toggle)
-      (define-key evil-normal-state-local-map (kbd "z") 'neotree-stretch-toggle)
-      (define-key evil-normal-state-local-map (kbd "R") 'neotree-refresh)
-      (define-key evil-normal-state-local-map (kbd "m") 'neotree-rename-node)
-      (define-key evil-normal-state-local-map (kbd "c") 'neotree-create-node)
-      (define-key evil-normal-state-local-map (kbd "d") 'neotree-delete-node)
+;; Indent guides 
+(use-package highlight-indent-guides
+    :ensure t
+    :general
+    (general-nmap :prefix my-leader
+                  "ig" 'highlight-indent-guides-mode)
+    :init
+    (setq highlight-indent-guides-method 'column)
+    (add-hook 'prog-mode-hook 'highlight-indent-guides-mode))
 
-      (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
+;; Rainbow color delimiters
+(use-package rainbow-delimiters
+    :general
+    (general-nmap :prefix my-leader
+                  "rb" 'rainbow-delimiters-mode)
+    :ensure t)
 
-; Helm
-;------------------------------------------------------------------------------
-; ;; Enable helm with default settings
-(require 'helm-config)
+;; NerdTree clone
+(use-package neotree
+    :ensure t
+    :general
+    (general-nmap :prefix my-leader
+                  "m" 'neotree-toggle)
+    :init
+    (add-hook 'neotree-mode-hook
+        (lambda ()
+          (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+          (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
+          (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+          (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)
+          (define-key evil-normal-state-local-map (kbd "I") 'neotree-hidden-file-toggle)
+          (define-key evil-normal-state-local-map (kbd "z") 'neotree-stretch-toggle)
+          (define-key evil-normal-state-local-map (kbd "R") 'neotree-refresh)
+          (define-key evil-normal-state-local-map (kbd "m") 'neotree-rename-node)
+          (define-key evil-normal-state-local-map (kbd "c") 'neotree-create-node)
+          (define-key evil-normal-state-local-map (kbd "d") 'neotree-delete-node))))
 
-; Javascript mode js2-mode
-;------------------------------------------------------------------------------
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+;; Alternative to tagbar
+(use-package sr-speedbar
+    :ensure t)
 
-; Python-mode
-;------------------------------------------------------------------------------
-(autoload 'python-mode "python-mode" "Python Mode." t)
- (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
- (add-to-list 'interpreter-mode-alist '("python" . python-mode))
+;; Helm
+(use-package helm
+    :general
+    (general-nmap :prefix my-leader
+                  "b" 'helm-buffers-list)
+    :ensure t
+    :diminish helm-mode
+    :config
+    (helm-autoresize-mode))
 
-; Jedi
-;------------------------------------------------------------------------------
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:complete-on-dot t)                 ; optional
-;; After this run jedi:install-server in emacs
+;; Project interaction
+(use-package projectile
+    :ensure t
+    :diminish projectile-mode
+    :init 
+    (projectile-mode))
 
-; Startup screen dashboard
-;------------------------------------------------------------------------------
-(require 'dashboard)
-(dashboard-setup-startup-hook)
+;; Helm + projectile
+(use-package helm-projectile
+    :ensure t
+    :config
+    (helm-projectile-on))
 
-; PHP
-;------------------------------------------------------------------------------
-(require 'php-mode)
-(eval-after-load 'php-mode
-  '(require 'php-ext))
+;; Helm ag
+(use-package helm-ag
+    :ensure t
+    :config
+    (setq helm-ag-fuzzy-match t))
 
-; Web Mode
-;------------------------------------------------------------------------------
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-; (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+;; Used for sorting in ivy
+(use-package flx
+    :ensure t)
+
+;; Ivy 
+(use-package ivy
+    :ensure t
+    :init
+    :general
+    (general-nmap :prefix my-leader
+                  "ss" 'swiper)
+    :config
+    (ivy-mode t)
+    ; (setq ivy-use-virtual-buffers t)
+    ;; Enable fuzzy matching for results
+    (setq ivy-re-builders-alist
+          '((t . ivy--regex-fuzzy)))
+    (setq enable-recursive-minibuffers t))
+
+(use-package counsel
+    :ensure t
+    :general
+    (general-nmap :prefix my-leader
+                  "a" 'counsel-ag)
+    (general-nmap "C-p" 'counsel-find-file)
+    ;; Use counsel for M-x
+    ;; faster than helm
+    :bind ("M-x" . counsel-M-x))
+
+;; Line numbers
+(use-package nlinum
+    :ensure t
+    :config
+    (add-hook 'prog-mode-hook 'nlinum-mode))
+
+;;; Language specific
+;; PHP
+(use-package php-mode
+    :ensure t
+    :init
+    (add-to-list 'auto-mode-alist '("\\.php$" . php-mode)))
+
+;; C, C++
+(use-package cc-mode
+    :ensure t)
+
+;; Web
+(use-package web-mode
+    :ensure t
+    :init
+    (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+    ; (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode)))
+
+;; Haskell
+(use-package haskell-mode
+    :ensure t)
+
+;; Python
+(use-package python-mode
+    :ensure t
+    :init
+    (autoload 'python-mode "python-mode" "Python Mode." t)
+    (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+    (add-to-list 'interpreter-mode-alist '("python" . python-mode)))
+
+; ;; Jedi
+; (use-package jedi
+;     :ensure t
+;     :init
+;     (add-hook 'python-mode-hook 'jedi:setup)
+;     :config ;     (setq jedi:complete-on-dot t))
+;     ;; After this run jedi:install-server in emacs
+
+;; Javascript
+(use-package js2-mode
+    :ensure t
+    :init
+    (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
+
+;;; Clojure
+(use-package clojure-mode
+    :ensure t
+    :init
+    ;; Use clojure mode for other extensions
+    (add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
+    (add-to-list 'auto-mode-alist '("\\.boot$" . clojure-mode))
+    (add-to-list 'auto-mode-alist '("\\.cljs.*$" . clojure-mode))
+    (add-to-list 'auto-mode-alist '("lein-env" . enh-ruby-mode))
+    :config
+    (add-hook 'clojure-mode-hook 'enable-paredit-mode)
+    (add-hook 'clojure-mode-hook 'subword-mode))
+
+(use-package clojure-mode-extra-font-locking
+    :ensure t)
+
+(use-package cider
+    :ensure t
+    :init
+    ;; go right to the REPL buffer when it's finished connecting
+    (setq cider-repl-pop-to-buffer-on-connect t)
+    ;; When there's a cider error, show its buffer and switch to it
+    (setq cider-show-error-buffer t)
+    (setq cider-auto-select-error-buffer t)
+    ;; Where to store the cider history.
+    (setq cider-repl-history-file "~/.emacs.d/cider-history")
+    ;; Wrap when navigating history.
+    (setq cider-repl-wrap-history t)
+    :config
+    ;; provides minibuffer documentation for the code you're typing into the repl
+    (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+    ;; enable paredit in your REPL
+    (add-hook 'cider-repl-mode-hook 'paredit-mode) )
+
+(use-package paredit
+    :ensure t)
+
+(provide 'modes)
+
+;;; modes.el ends here
